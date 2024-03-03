b7534611e12423583e923dbaf4161e48b6b5feba
HoleHolo <hola0_o@qq.com>
2024-03-03 12:37:51 +0800

    继续完善 WebUI

---
+++ webroot/js/app.js
@@ -0,0 +1,19 @@
+import { exec } from "kernelsu";
+
+function start_service () {
+
+}
+function kill_service () {
+
+}
+
+function apply_config (setting,value) {
+
+}
+
+function update_statu () {
+
+}
+function update_output () {
+
+}
