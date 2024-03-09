24df7903df3fe5fa222f4909d5d505effb25fd35
HoleHolo <hola0_o@qq.com>
2024-03-09 23:48:49 +0800

    修复 Bug 中

--- tools/kill.sh
+++ tools/kill.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 killall uperf
 pkill -f "/data/adb/ksu/bin/busybox sh /data/adb/modules/uperf_for_kirin970/service.sh"
+pkill -f "$(pwd)/../service.sh"
--- tools/start.sh
+++ tools/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-DIR=${0%/*}
-sh "$DIR/kill.sh"
-sh "$DIR/../service.sh"
+cd "$(pwd)/.."
+tools/kill.sh
+service.sh
